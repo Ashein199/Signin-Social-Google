5deeff6f04b920fa31ba97bb2e5fbf0a249be798
Ashein199 <heroglasses155@gmail.com>
2023-05-12 11:37:23 +0630

    sign in with google and facebook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleLogin } from "@react-oauth/google";
+import logo from "./logo.svg";
+import "./App.css";
+import { FacebookLoginButton } from "react-social-login-buttons";
+import { LoginSocialFacebook } from "reactjs-social-login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+
+        <LoginSocialFacebook
+          appId="201874862201176"
+          onResolve={(response) => {
+            console.log(response);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <FacebookLoginButton />
+        </LoginSocialFacebook>
+      </div>
     </div>
   );
 }
